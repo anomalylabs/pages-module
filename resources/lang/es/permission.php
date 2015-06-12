549ad999207e6e83a761127f3f8870da4a12f95d
Jose Luis Fonseca <jose@ditecnologia.com>
2015-06-12 09:17:08 -0500

    Spanish Translation

---
+++ resources/lang/es/permission.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'settings' => 'Configuración del módulo',
+    'pages'    => [
+        'label' => 'Administración de páginas'
+    ],
+    'types'    => [
+        'label' => 'Administración de tipos de páginas'
+    ]
+];
---
+++ resources/lang/es/setting.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'ttl' => [
+        'name'         => 'TTL',
+        'label'        => 'Tiempo de vida (TTL)',
+        'instructions' => '¿Por cuánto tiempo (en segundos) quiere almacenar en caché la página antes de servir el contenido actualizado?'
+    ],
+];
