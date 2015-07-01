a5e88c6013d7ac6669d18b58746be3876a6182c1
Ryan Thompson <service@aiwebsystems.com>
2015-07-01 09:38:12 -0500

    add

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -3,5 +3,5 @@
 return [
     'choose_type'       => 'What type of page would you like to create?',
     'choose_field_type' => 'What field type would you like to use?',
-    'choose_field'      => 'What field would you like to use?'
+    'choose_field'      => 'What field would you like to add?'
 ];
