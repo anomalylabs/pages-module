7e44599cb3b5f2d64f991538edabf74146b5b9ca
Brennon Loveless <brennon.loveless@gmail.com>
2015-07-22 21:50:59 -0600

    Add permissions and lanugage

---
+++ resources/lang/en/permission.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'pages' => [
+        'read' => 'Can read pages?',
+        'write' => 'Can write pages?',
+        'delete' => 'Can delete pages?',
+        'write_live' => 'Can write live pages?',
+        'make_live' => 'Can make draft page live?'
+    ],
+    'types'   => [
+        'read' => 'Can read types?',
+        'write' => 'Can write types?',
+        'delete' => 'Can delete types?',
+        'fields' => 'Can modify fields of types?'
+    ],
+    'fields'   => [
+        'read' => 'Can read fields?',
+        'write' => 'Can write fields?',
+        'delete' => 'Can delete fields?'
+    ]
+];
