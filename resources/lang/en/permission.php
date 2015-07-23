cddeed807f3fc82c054ee21cc06668f644e4c009
Brennon Loveless <brennon.loveless@gmail.com>
2015-07-22 22:05:39 -0600

    Add title translations

--- resources/lang/en/permission.php
+++ resources/lang/en/permission.php
@@ -2,6 +2,7 @@
 
 return [
     'pages'  => [
+        'title'      => 'Pages',
         'read'       => 'Can read pages?',
         'write'      => 'Can write pages?',
         'delete'     => 'Can delete pages?',
@@ -9,12 +10,14 @@ return [
         'make_live'  => 'Can make draft page live?'
     ],
     'types'  => [
+        'title'  => 'Types',
         'read'   => 'Can read types?',
         'write'  => 'Can write types?',
         'delete' => 'Can delete types?',
         'fields' => 'Can modify fields of types?'
     ],
     'fields' => [
+        'title'  => 'Fields',
         'read'   => 'Can read fields?',
         'write'  => 'Can write fields?',
         'delete' => 'Can delete fields?'
