faf5945f9d385d520a851dc78ab8fa11d6e4fe9e
Ryan Thompson <service@aiwebsystems.com>
2015-09-06 02:26:44 -0500

    Config for search

---
+++ resources/config/search.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Anomaly\PagesModule\Page\PageModel' => [
+        'default' => [
+            'enabled' => 'live',
+            'fields'  => [
+                'title'            => 'title',
+                'meta_title'       => 'meta_title',
+                'meta_description' => 'meta_description',
+                'meta_keywords'    => 'meta_keywords'
+            ]
+        ]
+    ]
+];
