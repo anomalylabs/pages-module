b679a589de8ae0a8b2c3d61a7f5050e62def3850
Ryan Thompson <ryan@pyrocms.com>
2016-01-30 21:35:05 -0600

    Shorter description

--- resources/lang/en/addon.php
+++ resources/lang/en/addon.php
@@ -3,7 +3,7 @@
 return [
     'title'       => 'Pages',
     'name'        => 'Pages Module',
-    'description' => 'A powerful module for the Streams Platform that lets you create pages, generate navigation, manage content, and build websites faster than ever.',
+    'description' => 'Create pages, generate navigation, manage content, and build websites faster than ever.',
     'section'     => [
         'pages'  => 'Pages',
         'types'  => 'Types',
