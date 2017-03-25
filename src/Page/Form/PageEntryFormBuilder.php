c83b737febdc55667a3cdcb4f215341623475e44
Ryan Thompson <ryan@pyrocms.com>
2017-03-24 23:49:58 -0500

    View button

--- src/Page/Form/PageEntryFormBuilder.php
+++ src/Page/Form/PageEntryFormBuilder.php
@@ -15,6 +15,20 @@ class PageEntryFormBuilder extends MultipleFormBuilder
 {
 
     /**
+     * The form buttons.
+     *
+     * @var array
+     */
+    protected $buttons = [
+        'cancel',
+        'view' => [
+            'enabled' => 'edit',
+            'target'  => '_blank',
+            'href'    => 'admin/pages/view/{request.route.parameters.id}',
+        ],
+    ];
+
+    /**
      * Fired after the entry form is saved.
      *
      * After the entry form is saved take the
