06970df0272860aba835f5213fe2f74aef5abaa4
Ryan Thompson <ryan@pyrocms.com>
2018-08-31 13:11:49 -0500

    YUS

--- migrations/2015_03_20_171947_anomaly.module.pages__create_pages_fields.php
+++ migrations/2015_03_20_171947_anomaly.module.pages__create_pages_fields.php
@@ -70,6 +70,7 @@ class AnomalyModulePagesCreatePagesFields extends Migration
         'theme_layout'     => [
             'type'   => 'anomaly.field_type.select',
             'config' => [
+                'mode'          => 'search',
                 'default_value' => 'theme::layouts/default.twig',
                 'handler'       => 'Anomaly\SelectFieldType\Handler\Layouts@handle',
             ],
