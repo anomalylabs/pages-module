32cc7dc89f73b10192a119b71a35abc273d96113
Ryan Thompson <service@aiwebsystems.com>
2019-01-02 10:07:10 -0600

    Bootstrap 3 and 4 presets

---
+++ src/PagesModuleCriteria.php
@@ -0,0 +1,46 @@
+<?php namespace Anomaly\PagesModule;
+
+use Anomaly\Streams\Platform\Addon\Plugin\PluginCriteria;
+
+/**
+ * Class PagesModuleCriteria
+ *
+ * @link   http://pyrocms.com/
+ * @author PyroCMS, Inc. <support@pyrocms.com>
+ * @author Ryan Thompson <ryan@pyrocms.com>
+ */
+class PagesModuleCriteria extends PluginCriteria
+{
+
+    /**
+     * Set options for Bootstrap 4
+     *
+     * @return $this
+     */
+    public function bootstrap4()
+    {
+        $this->options['link_attributes_dropdown'] = ['data-toggle' => 'dropdown'];
+        $this->options['list_class']               = 'nav justify-content-end';
+        $this->options['child_list_class']         = 'dropdown-menu';
+        $this->options['child_linkClass']          = 'dropdown-item';
+        $this->options['item_class']               = 'nav-item';
+        $this->options['link_class']               = 'nav-link';
+
+        return $this;
+    }
+
+    /**
+     * Set options for Bootstrap 3
+     *
+     * @return $this
+     */
+    public function bootstrap3()
+    {
+        $this->options['link_attributes_dropdown'] = ['data-toggle' => 'dropdown'];
+        $this->options['list_class']               = 'nav navbar-nav navbar-right';
+        $this->options['child_list_class']         = 'dropdown-menu';
+
+        return $this;
+    }
+    
+}
--- src/PagesModulePlugin.php
+++ src/PagesModulePlugin.php
@@ -4,7 +4,6 @@ use Anomaly\PagesModule\Page\Command\GetPage;
 use Anomaly\PagesModule\Page\Command\RenderNavigation;
 use Anomaly\PagesModule\Page\PageModel;
 use Anomaly\Streams\Platform\Addon\Plugin\Plugin;
-use Anomaly\Streams\Platform\Addon\Plugin\PluginCriteria;
 use Anomaly\Streams\Platform\Support\Collection;
 use Anomaly\Streams\Platform\Support\Decorator;
 
@@ -29,7 +28,7 @@ class PagesModulePlugin extends Plugin
             new \Twig_SimpleFunction(
                 'structure',
                 function ($root = null) {
-                    return (new PluginCriteria(
+                    return (new PagesModuleCriteria(
                         'render',
                         function (Collection $options) use ($root) {
 
