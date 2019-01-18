4d1a8208d516cba193e93bf75b2e414009375510
Ryan Thompson <service@aiwebsystems.com>
2019-01-18 01:36:50 -0600

    Simplify this

--- resources/config/sitemap.php
+++ resources/config/sitemap.php
@@ -1,41 +1,5 @@
 <?php
 
-use Anomaly\PagesModule\Page\Contract\PageInterface;
-use Anomaly\PagesModule\Page\Contract\PageRepositoryInterface;
-use Illuminate\Contracts\Config\Repository;
-use Roumen\Sitemap\Sitemap;
-
 return [
-    'lastmod' => function (PageRepositoryInterface $pages) {
-
-        $page = $pages->lastModified();
-
-        return $page->lastModified()->toAtomString();
-    },
-    'entries' => function (PageRepositoryInterface $pages) {
-        return $pages->accessible()->enabled();
-    },
-    'handler' => function (Sitemap $sitemap, Repository $config, PageInterface $entry) {
-
-        $translations = [];
-
-        foreach ($config->get('streams::locales.enabled') as $locale) {
-            if ($locale != $config->get('streams::locales.default')) {
-                $translations[] = [
-                    'language' => $locale,
-                    'url'      => url($locale . $entry->getPath()),
-                ];
-            }
-        }
-
-        $sitemap->add(
-            url($entry->getPath()),
-            $entry->lastModified()->toAtomString(),
-            0.5,
-            'monthly',
-            [],
-            null,
-            $translations
-        );
-    },
+    'pages' => \Anomaly\PagesModule\Page\Contract\PageRepositoryInterface::class,
 ];
