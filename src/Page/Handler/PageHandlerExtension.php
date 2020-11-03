f9d8e399d50e354121d3b5972003424209ff0488
Ryan Thompson <ryan@pyrocms.com>
2020-11-02 22:00:06 -0600

    Update

--- src/Page/Handler/PageHandlerExtension.php
+++ src/Page/Handler/PageHandlerExtension.php
@@ -39,7 +39,7 @@ class PageHandlerExtension extends Extension implements PageHandlerInterface
          * If the page is exact then
          * return it as is with no {any}.
          */
-        if ($page->isExact()) {
+        if ($page->isExact() && !$page->isHome()) {
             return implode(
                 "\n\n",
                 $translations->map(
