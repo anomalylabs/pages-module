c3da23be32ce553e41f741ebc1e05f77c524d2d4
Aidan Watt <aidan@hotfootdesign.co.uk>
2026-09-15 17:06:57 +0100

    PSA-PAG-03 guarded the pages Fields and Assignments controllers with the              resources/streams/config/navigation.php                          -11   fields.manage and types.fields permissions

--- src/Http/Controller/Admin/AssignmentsController.php
+++ src/Http/Controller/Admin/AssignmentsController.php
@@ -1,5 +1,7 @@
 <?php namespace Anomaly\PagesModule\Http\Controller\Admin;
 
+use Anomaly\Streams\Platform\Support\Authorizer;
+
 /**
  * Class AssignmentsController
  *
@@ -17,4 +19,23 @@ class AssignmentsController extends \Anomaly\Streams\Platform\Http\Controller\As
      */
     protected $namespace = 'pages';
 
+    /**
+     * Create a new AssignmentsController instance.
+     *
+     * @param Authorizer $authorizer
+     */
+    public function __construct(Authorizer $authorizer)
+    {
+        parent::__construct();
+
+        $this->middleware(
+            function ($request, $next) use ($authorizer) {
+                if (!$authorizer->authorize('anomaly.module.pages::types.fields')) {
+                    abort(403);
+                }
+
+                return $next($request);
+            }
+        );
+    }
 }
--- src/Http/Controller/Admin/FieldsController.php
+++ src/Http/Controller/Admin/FieldsController.php
@@ -1,5 +1,7 @@
 <?php namespace Anomaly\PagesModule\Http\Controller\Admin;
 
+use Anomaly\Streams\Platform\Support\Authorizer;
+
 /**
  * Class FieldsController
  *
@@ -17,4 +19,23 @@ class FieldsController extends \Anomaly\Streams\Platform\Http\Controller\FieldsC
      */
     protected $namespace = 'pages';
 
+    /**
+     * Create a new FieldsController instance.
+     *
+     * @param Authorizer $authorizer
+     */
+    public function __construct(Authorizer $authorizer)
+    {
+        parent::__construct();
+
+        $this->middleware(
+            function ($request, $next) use ($authorizer) {
+                if (!$authorizer->authorize('anomaly.module.pages::fields.manage')) {
+                    abort(403);
+                }
+
+                return $next($request);
+            }
+        );
+    }
 }
